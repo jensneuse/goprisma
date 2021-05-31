476ca09b780fd1ac3cbc56a7bc5912e745f33cac
Jens Neuse <jens.neuse@gmx.de>
2021-05-31 18:15:23 +0200

    update lib

--- cgo.go
+++ cgo.go
@@ -1,7 +1,6 @@
 package goprisma
 
 /*
-#cgo LDFLAGS: -pthread
 #cgo darwin,!arm64 LDFLAGS: -L"${SRCDIR}/lib/darwin" -lquery_engine_c_api
 #cgo darwin,arm64 LDFLAGS: -L"${SRCDIR}/lib/darwin-aarch64" -lquery_engine_c_api
 #cgo linux LDFLAGS: -L"${SRCDIR}/lib/linux" -lquery_engine_c_api
