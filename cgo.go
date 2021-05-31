00e9ed6e4bdeb034a8b3b6d1cc699d7f3f50b103
Jens Neuse <jens.neuse@gmx.de>
2021-05-31 23:20:49 +0200

    fix cgo flags

--- cgo.go
+++ cgo.go
@@ -1,8 +1,7 @@
 package goprisma
-
 /*
-#cgo darwin,!arm LDFLAGS: -L"${SRCDIR}/lib/darwin" -Wl,-rpath,"${SRCDIR}/lib/darwin" -lquery_engine_c_api
-#cgo darwin,arm LDFLAGS: -L"${SRCDIR}/lib/darwin-aarch64" -Wl,-rpath,"${SRCDIR}/lib/darwin" -lquery_engine_c_api
+#cgo darwin,!arm64 LDFLAGS: -L"${SRCDIR}/lib/darwin" -Wl,-rpath,"${SRCDIR}/lib/darwin" -lquery_engine_c_api
+#cgo darwin,arm64 LDFLAGS: -L"${SRCDIR}/lib/darwin-aarch64" -Wl,-rpath,"${SRCDIR}/lib/darwin-aarch64" -lquery_engine_c_api
 #cgo linux LDFLAGS: -L"${SRCDIR}/lib/linux" -lquery_engine_c_api
 #cgo windows LDFLAGS: -L"${SRCDIR}/lib/windows" -lquery_engine_c_api
 */
