39fb838fc3a7bed0cf2c1dc4b7de4aeb05ac9199
Jens Neuse <jens.neuse@gmx.de>
2021-06-01 14:29:40 +0200

    add dll for windows

--- cgo.go
+++ cgo.go
@@ -3,6 +3,6 @@ package goprisma
 #cgo darwin,!arm64 LDFLAGS: -L"${SRCDIR}/lib/darwin" -Wl,-rpath,"${SRCDIR}/lib/darwin" -lquery_engine_c_api
 #cgo darwin,arm64 LDFLAGS: -L"${SRCDIR}/lib/darwin-aarch64" -Wl,-rpath,"${SRCDIR}/lib/darwin-aarch64" -lquery_engine_c_api
 #cgo linux LDFLAGS: -L"${SRCDIR}/lib/linux" -Wl,-rpath,"${SRCDIR}/lib/linux" -lquery_engine_c_api
-#cgo windows LDFLAGS: -L"${SRCDIR}/lib/windows" -lquery_engine_c_api
+#cgo windows LDFLAGS: -L"${SRCDIR}/lib/windows" -Wl,-rpath,"${SRCDIR}/lib/darwin" -static -ldbghelp -lssp -lwinmm -lquery_engine_c_api
 */
 import "C"
